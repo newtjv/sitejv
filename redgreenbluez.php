7d9bd9c6439f9aa6e2f122a94d9056c7b1361d1d
Joao Vitor A H dos Santos <newtjv@gmail.com>
2020-12-07 22:55:14 -0300

    updates

--- redgreenbluez.php
+++ redgreenbluez.php
@@ -28,15 +28,15 @@
                         <p>Aero, Wild Ride City Zoo, Eyecandy, Cosmos 2, un, bliss, On/Off Shore, Mesa, Diamond</p>
                     </div>
                     <div class="gallery-9">
-                        <img src="assets/redgreenbluez/Aero.jpg" alt="Waves">   
-                        <img src="assets/redgreenbluez/WRCZ.jpg" alt="">
-                        <img src="assets/redgreenbluez/Eyecandy.jpg" alt="">
-                        <img src="assets/redgreenbluez/Cosmos2.jpg" alt="">
-                        <img src="assets/redgreenbluez/un.jpg" alt="">
-                        <img src="assets/redgreenbluez/bliss.jpg" alt="">
-                        <img src="assets/redgreenbluez/onoffshore.jpg" alt="">
-                        <img src="assets/redgreenbluez/Mesa.jpg" alt="">
-                        <img src="assets/redgreenbluez/Diamond.jpg" alt="">
+                        <a href="assets/redgreenbluez/Aero.jpg" data-lightbox="vol3" data-title="Aero"><img src="assets/redgreenbluez/Aero.jpg"></a>   
+                        <a href="assets/redgreenbluez/WRCZ.jpg" data-lightbox="vol3" data-title="Wild Ride City Zoo"><img src="assets/redgreenbluez/WRCZ.jpg"></a> 
+                        <a href="assets/redgreenbluez/Eyecandy.jpg" data-lightbox="vol3" data-title="eyecsndy"><img src="assets/redgreenbluez/Eyecandy.jpg"></a> 
+                        <a href="assets/redgreenbluez/Cosmos2.jpg" data-lightbox="vol3" data-title="Cosmos 2"><img src="assets/redgreenbluez/Cosmos2.jpg"></a> 
+                        <a href="assets/redgreenbluez/un.jpg" data-lightbox="vol3" data-title="un"><img src="assets/redgreenbluez/un.jpg"></a> 
+                        <a href="assets/redgreenbluez/bliss.jpg" data-lightbox="vol3" data-title="bliss"><img src="assets/redgreenbluez/bliss.jpg"></a> 
+                        <a href="assets/redgreenbluez/onoffshore.jpg" data-lightbox="vol3" data-title="On/Off Shore"><img src="assets/redgreenbluez/onoffshore.jpg"></a> 
+                        <a href="assets/redgreenbluez/Mesa.jpg" data-lightbox="vol3" data-title="Mesa"><img src="assets/redgreenbluez/Mesa.jpg"></a> 
+                        <a href="assets/redgreenbluez/Diamond.jpg" data-lightbox="vol3" data-title="Diamond Rush"><img src="assets/redgreenbluez/Diamond.jpg"></a> 
                     </div>
                 </div>                
                 <br>
@@ -47,10 +47,10 @@
                         <h4>2019</h4>
                     </div>
                     <div class="gallery-4">
-                        <img src="assets/redgreenbluez/bus.jpg" alt="">
-                        <img src="assets/redgreenbluez/bus 2.jpg" alt="">
-                        <img src="assets/redgreenbluez/bus 3.jpg" alt="">
-                        <img src="assets/redgreenbluez/bus 4.jpg" alt="">    
+                        <a href="assets/redgreenbluez/bus.jpg" data-lightbox="bus" data-title="bus"><img src="assets/redgreenbluez/bus.jpg" alt=""></a>
+                        <a href="assets/redgreenbluez/bus 2.jpg" data-lightbox="bus" data-title="bus"><img src="assets/redgreenbluez/bus 2.jpg" alt=""></a>
+                        <a href="assets/redgreenbluez/bus 3.jpg" data-lightbox="bus" data-title="bus"><img src="assets/redgreenbluez/bus 3.jpg" alt=""></a>
+                        <a href="assets/redgreenbluez/bus 4.jpg" data-lightbox="bus" data-title="bus"><img src="assets/redgreenbluez/bus 4.jpg" alt=""></a>    
                     </div>
                 </div>
                 <br>
@@ -61,8 +61,8 @@
                         <h4>2019</h4>
                     </div>
                     <div class="gallery-4">
-                        <img src="assets/redgreenbluez/Lightning Bold 2.jpg" alt="">
-                        <img src="assets/redgreenbluez/lightning bold 2 poster.jpg" alt="">
+                        <a href="assets/redgreenbluez/Lightning Bold 2.jpg" data-lightbox="lb" data-title="Lightning Bold: The Second Strike"><img src="assets/redgreenbluez/Lightning Bold 2.jpg" alt=""></a>
+                        <a href="assets/redgreenbluez/lightning bold 2 poster.jpg" data-lightbox="lb" data-title="Lightning Bold: The Second Strike"><img src="assets/redgreenbluez/lightning bold 2 poster.jpg" alt=""></a>
                     </div>
                 </div>                
                 <br>
@@ -74,15 +74,15 @@
                         <p>Life, Space Songz, Lightning Bold, Mint Condition, Don´t Drop The Name, Unknown, Answers to Unasked Questions, Ignite, Dimensional</p>
                     </div>
                     <div class="gallery-9">
-                        <img src="assets/redgreenbluez/Life.jpg" alt="Waves">   
-                        <img src="assets/redgreenbluez/Space Songs.jpg" alt="">
-                        <img src="assets/redgreenbluez/Lightning Bold.jpg" alt="">
-                        <img src="assets/redgreenbluez/Mint Condition.jpg" alt="">
-                        <img src="assets/redgreenbluez/Dont Drop.jpg" alt="">
-                        <img src="assets/redgreenbluez/Unknown.jpg" alt="">
-                        <img src="assets/redgreenbluez/Answers.jpg" alt="">
-                        <img src="assets/redgreenbluez/Ignite.jpg" alt="">
-                        <img src="assets/redgreenbluez/Dimensional.jpg" alt="">
+                        <a href="assets/redgreenbluez/Life.jpg" data-lightbox="vol2" data-title="un"><img src="assets/redgreenbluez/Life.jpg" alt="Waves"></a>    
+                        <a href="assets/redgreenbluez/Space Songs.jpg" data-lightbox="vol2" data-title="Space Songs"><img src="assets/redgreenbluez/Space Songs.jpg" alt=""></a> 
+                        <a href="assets/redgreenbluez/Lightning Bold.jpg" data-lightbox="vol2" data-title="Lightning Bold"><img src="assets/redgreenbluez/Lightning Bold.jpg" alt=""></a> 
+                        <a href="assets/redgreenbluez/Mint Condition.jpg" data-lightbox="vol2" data-title="Mint Condition"><img src="assets/redgreenbluez/Mint Condition.jpg" alt=""></a> 
+                        <a href="assets/redgreenbluez/Dont Drop.jpg" data-lightbox="vol2" data-title="Dont Drop"><img src="assets/redgreenbluez/Dont Drop.jpg" alt=""></a> 
+                        <a href="assets/redgreenbluez/Unknown.jpg" data-lightbox="vol2" data-title="Unknown"><img src="assets/redgreenbluez/Unknown.jpg" alt=""></a> 
+                        <a href="assets/redgreenbluez/Answers.jpg" data-lightbox="vol2" data-title="Answers"><img src="assets/redgreenbluez/Answers.jpg" alt=""></a> 
+                        <a href="assets/redgreenbluez/Ignite.jpg" data-lightbox="vol2" data-title="Ignite"><img src="assets/redgreenbluez/Ignite.jpg" alt=""></a> 
+                        <a href="assets/redgreenbluez/Dimensional.jpg" data-lightbox="vol2" data-title="Dimensional"><img src="assets/redgreenbluez/Dimensional.jpg" alt=""></a> 
                     </div>
                 </div>
                 <br>
@@ -94,15 +94,15 @@
                         <p>Waves, Mirage, Copacabana Mar Yogurt Kisses, Fleur de Printemps, Life´s a Beach, Guardando le Stelle, Voilá!, Red Green Bluez, Echosystem</p>
                     </div>
                     <div class="gallery-9">
-                        <img src="assets/redgreenbluez/Waves.JPG" alt="Waves">   
-                        <img src="assets/redgreenbluez/Mirage.JPG" alt="">
-                        <img src="assets/redgreenbluez/CMYK.jpg" alt="">
-                        <img src="assets/redgreenbluez/Fleur.JPG" alt="">
-                        <img src="assets/redgreenbluez/Lifes a Beach.JPG" alt="">
-                        <img src="assets/redgreenbluez/Stelle.JPG" alt="">
-                        <img src="assets/redgreenbluez/Voila.JPG" alt="">
-                        <img src="assets/redgreenbluez/RGB.JPG" alt="">
-                        <img src="assets/redgreenbluez/Echosystem.JPG" alt="">
+                        <a href="assets/redgreenbluez/Waves.JPG" data-lightbox="vol1" data-title="Waves"><img src="assets/redgreenbluez/Waves.JPG" alt="Waves"></a>    
+                        <a href="assets/redgreenbluez/Mirage.JPG" data-lightbox="vol1" data-title="Mirage"><img src="assets/redgreenbluez/Mirage.JPG" alt=""></a> 
+                        <a href="assets/redgreenbluez/CMYK.jpg" data-lightbox="vol1" data-title="CMYK"><img src="assets/redgreenbluez/CMYK.jpg" alt=""></a> 
+                        <a href="assets/redgreenbluez/Fleur.JPG" data-lightbox="vol1" data-title="Fleur"><img src="assets/redgreenbluez/Fleur.JPG" alt=""></a> 
+                        <a href="assets/redgreenbluez/Lifes a Beach.JPG" data-lightbox="vol1" data-title="Lifes a Beach"><img src="assets/redgreenbluez/Lifes a Beach.JPG" alt=""></a> 
+                        <a href="assets/redgreenbluez/Stelle.JPG" data-lightbox="vol1" data-title="Stelle"><img src="assets/redgreenbluez/Stelle.JPG" alt=""></a> 
+                        <a href="assets/redgreenbluez/Voila.JPG" data-lightbox="vol1" data-title="Voila"><img src="assets/redgreenbluez/Voila.JPG" alt=""></a> 
+                        <a href="assets/redgreenbluez/RGB.JPG" data-lightbox="vol1" data-title="RGB"><img src="assets/redgreenbluez/RGB.JPG" alt=""></a> 
+                        <a href="assets/redgreenbluez//Echosystem.JPG" data-lightbox="vol1" data-title="Echosystem"><img src="assets/redgreenbluez/Echosystem.JPG" alt=""></a> 
                     </div>
                 </div> 
                 <br>
